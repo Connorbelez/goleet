51ea5edfa2a80d9766003073d6207dabc47d0f4c
Connor Beleznay <67024888+Connorbelez@users.noreply.github.com>
2023-10-18 13:18:07 -0400

    test

--- balancedBinaryTree.go
+++ balancedBinaryTree.go
@@ -1,1 +1,41 @@
 package main
+
+import "math"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func balanced(root *TreeNode) (int, bool) {
+	if root == nil {
+		return 0, true
+	}
+	h := 1
+	left, right := 0, 0
+	lb, rb := true, true
+
+	left, lb = balanced(root.Left)
+	right, rb = balanced(root.Right)
+
+	h = h + int(math.Max(float64(left), float64(right)))
+	if !(rb && lb) {
+		return -1, false
+	}
+
+	return h, math.Abs(float64(left-right)) <= 1
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	_, b := balanced(root)
+
+	return b
+}
+
+func main() {
+	isBalanced(&TreeNode{Val: 10})
+}
